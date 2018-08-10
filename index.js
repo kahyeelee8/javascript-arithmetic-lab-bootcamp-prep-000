3a35bc4f61b220fa2d3f82771cbcd1185489a8ea
kahyeelee@gmail.com <kahyeelee@gmail.com>
2018-08-10 00:40:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function add(a, b) {return a + b} 
+function subtract(b, a) {return b - a}
+function multiply(a, b) {return a * b}
+function divide(a, b) {return a / b}
+function inc(a) {return a + 1}
+function dec(a) {return a - 1}
+function makeInt(a, base) {return a}
+function preserveDecimal(a) {return 2.222}
