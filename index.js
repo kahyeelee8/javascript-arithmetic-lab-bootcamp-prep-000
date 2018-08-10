b9cadd1d9f94732ff366b52561ff62c6553378bd
kahyeelee@gmail.com <kahyeelee@gmail.com>
2018-08-10 00:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function divide(a, b) {return a / b}
 function inc(a) {return a + 1}
 function dec(a) {return a - 1}
 function makeInt(n) {
-  return parseInt(n, 10)
-}
+  return parseInt(n, 10) }
+function preserveDecimal(n) {return parseFloat(n)}
